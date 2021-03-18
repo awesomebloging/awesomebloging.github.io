dab8a89d0480901b94047c5e429f39dd6c263d5f
dev <user@users.noreply.github.com>
2021-03-19 02:51:24 +0300

    s

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "LolMiner&#58; Скачать, Настройка и Команды [2021]",
+        "excerpt":"Описаны все команды и аргументы командной строки Феникс Майнера   ","categories": ["miners"],
+        "tags": [],
+        "url": "https://miningsoft.org/miners/lolminer/",
+        "teaser": "https://miningsoft.org/assets/images/thumbs/500x300.png"
+      },]
